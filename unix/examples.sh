bbd6ae16d8746a6f0f7f0bf6a346e0b8a30b35bd
sameer-kewal <156670963+sameer-kewal@users.noreply.github.com>
2024-05-05 04:23:07 +0200

    test

---
+++ unix/examples.sh
@@ -0,0 +1,12 @@
+LIQUIBASE UPDATE -CHANGELOG_FILE sta_announcement_table.xml -default_schema_name autos  -liquibase_schema_name autos -output_default_schema true
+
+APEX_2877897000694406
+
+
+LIQUIBASE UPDATE -CHANGELOG_FILE sta_announcement_table.xml -default_schema_name autos  -liquibase_schema_name autos -output_default_schema true -liquibase_tablespace_name APE
+
+
+
+set ddl partitioning off
+set ddl segment_attributes off
+set ddl tablespace off
