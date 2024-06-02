c2665f14351c2f5e3eea255429466ec721a9d66d
sameerkewal <sameerkewal1@gmail.com>
2024-06-02 16:11:00 -0300

    changes

--- data/start.sql
+++ data/start.sql
@@ -1,3 +1,7 @@
+-- Start spooling
+SPOOL my_output_file.log
+
+-- Your SQL script files
 @STA_ANNOUNCEMENT.sql
 @STA_AUTHORIZATION.sql
 @STA_PRIVILEGE.sql
@@ -5,7 +9,6 @@
 @STA_GRADE.sql
 @STA_COURSE.sql
 @STA_CLASS.sql
-@STA_COURSE_CLASS_TEACHER.sql
 @STA_COURSE_GRADE.sql
 @STA_ROLE.sql
 @STA_ROLE_PRIVILEGE.sql
@@ -13,6 +16,10 @@
 @STA_SYSTEM_PARAMETER.sql
 @STA_TEST.sql
 @STA_USER.sql
+@STA_COURSE_CLASS_TEACHER.sql
 @STA_STUDENT_REMARK.sql
 @STA_STUDENT_TEST.sql
 @STA_USER_ROLE.sql
+
+-- Stop spooling
+SPOOL OFF
